f72818468287c94e1d90b61dc1642e6284ccb9dd
Mr Moore <mooredevelopment2ks@gmail.com>
2025-11-16 17:13:26 +1100

    updated version in headers as required by foursquare

--- app/api/route.js
+++ app/api/route.js
@@ -2,12 +2,17 @@ const options = {
   headers: {
     'accept': "application/json",
     'Authorization': `fsq3 ${process.env.NEXT_PUBLIC_FSQR_KEY}`,
-    'Accept-Version': 'v2025-02-05'
+    'X-Places-Api-Version': '2025-02-05'
   }
 };
 
 const getDinnerUrl = (latitude, longitude, category) => {
-  return `https://places-api.foursquare.com/places/search?ll=${latitude}%2C${longitude}&radius=10000&categories=${category}&fields=name%2Clocation%2Ctel%2Cwebsite%2Chours%2Crating%2Cprice%2Cphotos&open_now=true&sort=DISTANCE&limit=10`;
+  // Default in case it can't find anything.
+  const lat = latitude || '0';
+  const long = longitude || '0';
+  const cate = category || '13000';
+
+  return `https://places-api.foursquare.com/places/search?ll=${lat}%2C${long}&radius=10000&categories=${cate}&fields=name%2Clocation%2Ctel%2Cwebsite%2Chours%2Crating%2Cprice%2Cphotos&open_now=true&sort=DISTANCE&limit=10`;
 };
 
 export async function GET(request) {
@@ -19,9 +24,16 @@ export async function GET(request) {
   );
   try{
     const apiResponse = await fetch(url, options);
+    if(!apiResponse.ok) {
+      const errorData = await apiResponse.json();
+      console.error(`Foursquare API Error (${apiResponse.status}):`, errorData);
+      return Response.json({ results: [], error: `API failed with status ${apiResponse.status}` }, { status: 200 });
+    }
     const data = await apiResponse.json()
     return Response.json(data)
   } catch(error) {
-    return Response.json([]);
+    console.error("Server-side fetching error:", error);
+
+    return Response.json({ results: [], error: 'Internal Server Error during fetch' }, { status: 500 });
   }
 }
